397869546f392e8a5b2d8de3333805dcd1174a61
luddelex <35630379+luddelex@users.noreply.github.com>
2019-03-04 12:12:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,26 +10,26 @@ const Auth = new AuthService();
 class App extends Component {
   render() {
     return (
-      <div className="App">
+        <div className="App">
 
-      <Header />
+          <Header />
 
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-          integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
-          crossorigin="anonymous"
-        />
+          <link
+              rel="stylesheet"
+              href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+              integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+              crossorigin="anonymous"
+          />
 
-        <ScreeningForm />
-      </div>
+          <ScreeningForm />
+        </div>
     );
   }
 
   handleLogout(){
     Auth.logout()
     this.props.history.replace('/login');
- }
+  }
 }
 
 export default withAuth(App);
